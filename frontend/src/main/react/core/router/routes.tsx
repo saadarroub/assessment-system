9e22e05a684ba718f10e766a17c33ba5f8c012fa
jayIsThere <jaehan.kim@studmail.w-hs.de>
2025-10-06 11:09:35 +0200

    Add Total Result Page

--- frontend/src/main/react/core/router/routes.tsx
+++ frontend/src/main/react/core/router/routes.tsx
@@ -6,6 +6,7 @@ import WorkerDashboard from '@/features/worker-area/WorkerDashboard';
 import AdminDashboard from '@/features/admin-area/AdminDashboard';
 import ProtectedRoute from './ProtectedRoute';
 import ResultsPage from '@/features/worker-area/results/ResultsPage';
+import TotalResultsPage from '@/features/worker-area/results/TotalResultsPage';
 
 // ⬇️ Topic-Seiten
 import EamPage from '@/features/admin-area/topics/eam/EamPage';
@@ -32,6 +33,14 @@ export default function AppRoutes() {
         element={
           <ProtectedRoute>
             <ResultsPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/app/totalresults/:sessionId"
+        element={
+          <ProtectedRoute>
+            <TotalResultsPage />
           </ProtectedRoute>
         }
       />
