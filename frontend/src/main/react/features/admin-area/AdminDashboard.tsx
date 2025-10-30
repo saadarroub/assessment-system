0eed6f10fd7373ce3949f17c3b56923d6e177bcc
Houcine El Boukhrissi <houcine@example.com>
2025-10-30 16:26:52 +0100

    Implementiert Frage-Hierarchie mit dynamischem Laden von Unterfragen

--- frontend/src/main/react/features/admin-area/AdminDashboard.tsx
+++ frontend/src/main/react/features/admin-area/AdminDashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import {  useNavigate } from "react-router-dom";
 import "@/styles/admin.css";
 import AdminLayout from "@/apps/app/AdminLayout";
