45e062f488f115627a0612c583c9a70b5cfc7c96
Houcine El Boukhrissi <houcine@example.com>
2025-11-30 22:16:04 +0100

    Ayman Test

--- frontend/src/main/react/features/admin-area/AdminDashboard.tsx
+++ frontend/src/main/react/features/admin-area/AdminDashboard.tsx
@@ -324,7 +324,7 @@ export default function AdminDashboard() {
       
   title="Fragenkatalog Administration"
   subtitle="Verwalten Sie Ihre Themen und erstellen Sie finale Kataloge"
-  icon={<Network  size={40} />}
+  icon={<Network  size={40 } />}
   gradient="navy"
   height="280px"
   showPattern={true}
